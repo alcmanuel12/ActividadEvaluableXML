0a16b57eea56a13ba3910fc1d698b7491b27e414
Manuel Alcántara Pelicano <155097722+alcmanuel12@users.noreply.github.com>
2025-03-03 22:04:53 +0100

    Update MarcadoresSAX.java

--- MarcadoresSAX.java
+++ MarcadoresSAX.java
@@ -1,72 +1,74 @@
-package marcadores;
+package ae_parsers;
 
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class MarcadoresSAX {
 	public static void main(String[] args) {
         try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
-            File outputFile = new File("analisis_sax.xml");
-            FileWriter writer = new FileWriter(outputFile);
-            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<analisis>\n");
-
-            DefaultHandler handler = new DefaultHandler() {
-                int level = 0;
-                boolean isEvento = false;
-
-                @Override
-                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-                    for (int i = 0; i < level; i++) System.out.print("  ");
-                    System.out.println(qName);
-                    level++;
-
-                    if (qName.equals("evento")) {
-                        isEvento = true;
-                        String id = attributes.getValue("id");
-                        String nombre = attributes.getValue("nombre");
-                        System.out.println("Evento - ID: " + id + ", Nombre: " + nombre);
-                        try {
-                            writer.write("  <evento id=\"" + id + "\" nombre=\"" + nombre + "\"/>\n");
-                        } catch (IOException e) {
-                            throw new SAXException("Error al escribir en el archivo: " + e.getMessage());
-                        }
-                    }
-                }
-
-                @Override
-                public void endElement(String uri, String localName, String qName) throws SAXException {
-                    level--;
-                    if (qName.equals("marcadores")) {
-                        try {
-                            writer.write("</analisis>");
-                            writer.close();
-                            System.out.println("Archivo 'analisis_sax.xml' generado correctamente.");
-                        } catch (IOException e) {
-                            throw new SAXException("Error al cerrar el archivo: " + e.getMessage());
-                        }
-                    }
-                }
-            };
-
+            
+            EventHandler handler = new EventHandler();
             saxParser.parse(new File("marcadores.xml"), handler);
-
+            
+            // Mostrar eventos encontrados
+            System.out.println("\nEventos encontrados:");
+            for (Evento evento : handler.getEventos()) {
+                System.out.println("Evento - ID: " + evento.id + ", Nombre: " + evento.nombre);
+            }
+            
         } catch (ParserConfigurationException e) {
-            System.err.println("Error de configuración del parser: " + e.getMessage());
+            System.err.println("Error en la configuración del parser: " + e.getMessage());
         } catch (SAXException e) {
-            System.err.println("Error en el procesamiento SAX: " + e.getMessage());
+            System.err.println("Error SAX: " + e.getMessage());
         } catch (IOException e) {
             System.err.println("Error de entrada/salida: " + e.getMessage());
         }
+    }
+}
+
+//Clase manejadora de eventos SAX
+class EventHandler extends DefaultHandler {
+    private int nivel = 0;
+    private ArrayList <Evento> eventos = new ArrayList<>();
+    
+ // Método que se llama al inicio de cada elemento
+    public void IncioElemento(String uri, String localName, String qName, Attributes attributes) {
+        String indent = "  ".repeat(nivel);
+        System.out.println(indent + "Nodo: " + qName);
+        
+        if (qName.equals("evento")) {
+            String id = attributes.getValue("id");
+            String nombre = attributes.getValue("nombre");
+            eventos.add(new Evento(id, nombre));
+        }
+        nivel++;
+    }
+ // Método que se llama al final de cada elemento
+    public void ElementoFinal(String uri, String localName, String qName) {
+        nivel--;
+    }
+    
+    public ArrayList<Evento> getEventos() {
+        return eventos;
+    }
+}
+
+class Evento {
+    String id;
+    String nombre;
+    
+    Evento(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
     }
 
 }
